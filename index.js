07a25aa34f3b93443cce62a20a8ec6c7c989788f
Cherkes001 <teuchezh666@yandex.ru>
2020-08-20 16:28:05 +0300

    next

--- index.js
+++ index.js
@@ -1,29 +1,69 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const fs = require("fs");
+const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
-
-app.get('/api/hello', function (req, res) {
-    //res.send('zdarova ebat');
-    res.send(
-        {
-            "success": "true",
-            "data": "world"
-        });
+app.get("/", function (req, res) {
+  res.send("Hello World");
 });
 
-app.get('/api/get-config', function (req, res) {
-    res.send(
-        {
-        "success": "true",
-        "data": {
-            "port": 'Server port: ' + listener.address().port,
-            "host": "Server adress is: " + listener.address().address,
-            }
-        });
+app.get("/api/hello", function (req, res) {
+  res.send({
+    success: true,
+    data: "world",
+  });
 });
 
-let listener = app.listen(3000, "127.0.0.1")
+app.get("/api/get-config", function (req, res) {
+  res.send({
+    success: true,
+    data: {
+      port: "Server port: " + listener.address().port,
+      host: "Server adress is: " + listener.address().address,
+    },
+  });
+});
 
+app.get("/api/learn", function (req, res) {
+  if (req.query.queryParam) {
+    res.send({
+      success: true,
+      data: req.query.queryParam,
+    });
+  } else {
+    res.send({
+      success: false,
+    });
+  }
+});
+
+app.get("/api/learn/path/:pathParam", function (req, res) {
+  if (req.params.pathParam) {
+    res.send({
+      success: true,
+      data: req.params.pathParam,
+    });
+  } else {
+    res.send({
+      success: false,
+    });
+  }
+});
+
+app.post("/api/store-text/:value", function (req, res) {
+  res.send({
+    success: true,
+    data: req.params.value,
+  });
+
+  const file = fs.createWriteStream("text.txt");
+  file.write(req.params.value);
+  file.end();
+  /*
+  const file = fs.createWriteStream("text.txt");
+  file.once("open", function (fd) {
+    file.write(req.params.value + "\n");
+    file.end();
+  });*/
+});
+
+let listener = app.listen(3000, "127.0.0.1");
