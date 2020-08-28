3f6293bf6b42dfff31df480258e61910d4aadbc9
Cherkes001 <teuchezh666@yandex.ru>
2020-08-28 15:51:54 +0300

    Проектирование и описание

---
+++ src/routes/api/communityapi.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const communityController = require('../../controllers/db-controller.js');
+const communitycontroller = new communityController();
+
+router.get();
+
+module.exports = router;
