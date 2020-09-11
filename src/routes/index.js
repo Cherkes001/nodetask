0f7a06957f62862478b86e24dbbfc50ab01b8fc4
Cherkes001 <teuchezh666@yandex.ru>
2020-09-11 09:26:54 +0300

    add allow access to headers

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use((req, res, next) => {
+  res.header('Access-Control-Expose-Headers', '*');
   res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Content-Type, *');
